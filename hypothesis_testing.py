3152b269f89b9ce465caf7bffd66db45d94aae4c
Soapony <seanponyfung@gmail.com>
2023-10-12 00:50:33 +1300

    a

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -2,14 +2,18 @@ import numpy as np
 from scipy import stats
 
 if __name__ == "__main__":
-    f=open("result/paper_zipf_hitrate.txt","r")
+    #f=open("result/paper_zipf_hitrate.txt","r")
+    f=open("result/paper_varPop_hitrate.txt","r")
+    #f=open("result/paper_varNor_hitrate.txt","r")
     line=f.readline()
     paper_hitrate = line.split(" ")
     paper_hitrate = paper_hitrate[:-1]
     paper_hitrate = np.array([float(x) for x in paper_hitrate])
     f.close()
 
-    f=open("result/new_zipf_hitrate.txt","r")
+    #f=open("result/new_zipf_hitrate.txt","r")
+    f=open("result/new_varPop_hitrate.txt","r")
+    #f=open("result/new_varNor_hitrate.txt","r")
     line=f.readline()
     new_hitrate = line.split(" ")
     new_hitrate = new_hitrate[:-1]
